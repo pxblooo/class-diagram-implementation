9c86e6243df43e58d95b03da6d964875351f2468
Djannn <djan_matthew_catibog@dlsl.edu.ph>
2025-03-01 09:16:41 +0800

    firstcomm

--- class-diagram-implementation.cpp
+++ class-diagram-implementation.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+class Product {
+    public:
+        int productID;
+        string name;
+        int price;
+        int stockQuantity;
+    
+        Product() {}
+        Product(int id, string n, int p, int stock) : productID(id), name(n), price(p), stockQuantity(stock) {}
+    };
+    
+    class ShoppingCart {
+    public:
+        Product products[10];
+        int quantities[10];
+        int totalAmount = 0;
+        int count = 0;
 
 
 int main(){
